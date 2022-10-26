3363e86d9f017142f004a0171e287819554a7eb5
dave-rushabh <rushabh.dave24@gmail.com>
2022-10-26 17:07:20 +0530

    Project setup with eslint, prettier and tailwind

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App({ test, test2 }) {
+    return (
+        <>
+            <div className='App'>Expenses Tracker</div>
+            <h1 class='text-3xl font-bold underline'>Hello world!</h1>
+        </>
+    )
+}
+
+export default App
