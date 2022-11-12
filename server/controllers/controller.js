7f4b83db5208ccabd201f8eb04af4393a3ed4ffa
dave-rushabh <rushabh.dave24@gmail.com>
2022-11-12 18:30:52 +0530

    APIs created

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -32,7 +32,87 @@ const getCategories = asyncHandler(async (req, res) => {
   }
 })
 
+// used to post transactions
+
+const createTransaction = asyncHandler(async (req, res) => {
+  try {
+    if (!req.body) return res.status(400).json('No data provided')
+    const { name, type, amount } = req.body
+
+    const data = await Transactions.create({
+      name,
+      type,
+      amount,
+      date: new Date(),
+    })
+    if (data) {
+      res.json(data)
+    }
+  } catch (error) {
+    res.status(400)
+    throw new Error(error)
+  }
+})
+
+// used to get all transactions
+const getTransactions = asyncHandler(async (req, res) => {
+  try {
+    const allTransactions = await Transactions.find({})
+    if (allTransactions) {
+      res.json(allTransactions)
+    }
+  } catch (error) {
+    res.status(400)
+    throw new Error(error)
+  }
+})
+
+// used to delete transaction by ID
+const deleteTransactionById = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params
+    console.log(id)
+
+    const data = await Transactions.findByIdAndDelete(id)
+
+    if (data) {
+      res.json(data)
+      // call get api from frontend after deleting the transaction
+    }
+  } catch (error) {
+    res.status(400)
+    throw new Error(error)
+  }
+})
+
+// now we want to join the Transactions Model and Categories Modal and retreive field like below:
+const getLabels = asyncHandler(async (req, res) => {
+  // lookup from one collection to merge into another collection
+  Transactions.aggregate([
+    {
+      $lookup: {
+        from: 'Categories',
+        localField: 'type',
+        foreignField: 'type',
+        as: 'Categories_info',
+      },
+    },
+    {
+      $unwind: '$Categories_info',
+    },
+  ])
+    .then(result => {
+      res.json(result)
+      console.log('try block works')
+    })
+    .catch(err => res.status(400).json('lookup failed'))
+})
+
 module.exports = {
   postCategories,
   getCategories,
+  createTransaction,
+  getTransactions,
+  deleteTransactionById,
+  getLabels,
 }
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,7 +1,17 @@
 const router = require('express').Router()
-const { getCategories, postCategories } = require('../controllers/controller')
+const {
+  getCategories,
+  postCategories,
+  createTransaction,
+  getTransactions,
+  deleteTransactionById,
+  getLabels,
+} = require('../controllers/controller')
 
 //creating the endpoints
 router.route('/categories').get(getCategories).post(postCategories)
+router.route('/transaction').post(createTransaction).get(getTransactions)
+router.route('/transaction/:id').delete(deleteTransactionById)
+router.route('/labels').get(getLabels)
 
 module.exports = router
